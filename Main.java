30e57f369b503f6fe4a498123a1dace93eeadf92
djuarez-2017510 <djuarez-2017510@kinal.edu.gt>
2024-01-29 09:58:31 -0600

    Se agrego el menu

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 import java.io.*;
 import java.util.*;
 import java.lang.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         GnomeSort<Integer> gnomeSort = new GnomeSort<>();
-
-
+        Scanner scanner = new Scanner(System.in);
 
         try {
             FileWriter writer = new FileWriter("random.txt");
@@ -32,11 +32,11 @@ public class Main {
             File archivo = new File("random.txt");
 
             // Crear un objeto Scanner para leer el archivo
-            Scanner scanner = new Scanner(archivo);
+            Scanner filescanner = new Scanner(archivo);
 
             // Leer el archivo línea por línea y agregar los números al array
-            while (scanner.hasNextLine() && indice < 3000) {
-                String linea = scanner.nextLine();
+            while (filescanner.hasNextLine() && indice < 3000) {
+                String linea = filescanner.nextLine();
                 int numero = Integer.parseInt(linea);
                 numerosArray[indice] = numero;
                 indice++;
@@ -47,14 +47,42 @@ public class Main {
             e.printStackTrace();
         }
 
+        while (true) {
+            System.out.println("\n*** Menú ***");
+            System.out.println("\nEliga el sort que desea utilizar");
+            System.out.println("1. Gnome sort");
+            System.out.println("2. Merge sort");
+            System.out.println("3. Quick sort");
+            System.out.println("4. Radix sort");
+            System.out.println("5. Bucket sort");
+            System.out.println("0. Salir");
 
-
-        gnomeSort.sort(numerosArray);
+            try {
+                
+                int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                gnomeSort.sort(numerosArray);
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 0:
+                System.out.println("Saliendo del programa");
+                System.exit(0);
+                default:
+                    System.out.println("No es una opción valida");
+            }
+            } catch (Exception e) {
+                System.out.println("Debe ingresar un valor numerico");
+                scanner.nextLine();
+            }
+        }
     }
-
-
-
-
-
-    }
+}
 
